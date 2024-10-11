258fe7881ce9bca0ee2ac574d08ebbfbb0ea1478
muddassiryahya <m.m2442212@gmail.com>
2024-10-11 16:09:56 +0500

    changes some lines

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -104,31 +104,6 @@ function HomePage() {
         setFiles([]);
     }
 
-    // const downloadAll = () => {
-    //     let filename = "AllFiles";
-    //     const urls = files.map(v => v.url)
-    //     const zip = new JSZip()
-    //     const folder = zip.folder('project')
-    //     urls.forEach((url) => {
-    //         const blobPromise = fetch(url)
-    //             .then(function (response) {
-    //                 console.log({ response })
-    //                 if (response.status === 200 || response.status === 0) {
-    //                     return Promise.resolve(response.blob());
-    //                 } else {
-    //                     return Promise.reject(new Error(response.statusText));
-    //                 }
-    //             })
-    //         const name = url.substring(url.lastIndexOf('/'))
-    //         folder.file(name, blobPromise)
-    //     })
-
-    //     zip.generateAsync({ type: "blob" })
-    //         .then(blob => saveAs(blob, filename))
-    //         .catch(e => console.log(e));
-
-
-    // }
 
     useEffect(() => {
         const textRef = ref(db, 'text-sharing');
